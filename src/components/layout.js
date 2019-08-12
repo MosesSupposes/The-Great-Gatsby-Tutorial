54b002ce63ea2503a9f064a45b34468a4e832656
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-11 20:01:10 -0700

    center text on all pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,10 +3,11 @@ import React from 'react'
 import Header from './header'
 import Footer from './footer'
 import '../styles/index.scss'
+import layoutStyles from './layout.module.scss'
 
 export default function Layout(props) {
     return (
-        <div>
+        <div className={layoutStyles.container}>
             <Header />
             {props.children}
             <Footer />
