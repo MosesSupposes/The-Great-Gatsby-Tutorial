57f30a247ef07f3d7450ff08fc3ab9453d05c875
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-11 20:28:32 -0700

    style navigation items

--- src/components/header.js
+++ src/components/header.js
@@ -14,11 +14,11 @@ export default function Header() {
                 </Link>
             </h1>
             <nav>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/about'>About</Link></li>
-                    <li><Link to='/blog'>Blog</Link></li>
-                    <li><Link to='/contact'>Contact</Link></li>
+                <ul className={headerStyles.navList}>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>Home</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about'>About</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/blog'>Blog</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/contact'>Contact</Link></li>
                 </ul>
             </nav>
         </header>
