41962e95f472f76b39bcc7a261ededbe6a7ae84e
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-11 17:59:30 -0700

    create Layout component

---
+++ src/components/layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Header from './header'
+import Footer from './footer'
+
+
+export default function Layout(props) {
+    return (
+        <div>
+            <Header />
+            {props.children}
+            <Footer />
+        </div>
+    )
+}
