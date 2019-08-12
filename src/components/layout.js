df1ce907206c0a486b9c1c20605c847e6b591094
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-11 20:03:51 -0700

    make the footer stick to the bottom

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,10 @@ import layoutStyles from './layout.module.scss'
 export default function Layout(props) {
     return (
         <div className={layoutStyles.container}>
-            <Header />
-            {props.children}
+            <div className={layoutStyles.content}>
+                <Header />
+                {props.children}
+            </div>
             <Footer />
         </div>
     )
