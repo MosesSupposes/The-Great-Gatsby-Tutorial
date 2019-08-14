057c77c8867b82f14a550bccdc84f225620f997f
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-13 17:31:25 -0700

    link to blog posts by slug

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout'
 
@@ -13,6 +13,9 @@ export default function BlogPage() {
                         frontmatter {
                             title
                             date
+                        }
+                        fields {
+                            slug
                         }
                     }
                 }
@@ -28,8 +31,10 @@ export default function BlogPage() {
                     {allPosts.allMarkdownRemark.edges.map(function renderBlogPostPreview(edge) {
                         return (
                             <li>
-                                <h2>{edge.node.frontmatter.title}</h2>
-                                <p>{edge.node.frontmatter.date}</p>
+                                <Link to={`/blog/${edge.node.fields.slug}`}>
+                                    <h2>{edge.node.frontmatter.title}</h2>
+                                    <p>{edge.node.frontmatter.date}</p>
+                                </Link>
                             </li>
                         )
                     })}
