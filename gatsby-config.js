ad64a454aef0612a4cdbe7583d48507d0743d083
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-15 14:09:17 -0700

    set up config for sourcing content from contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,12 @@
 module.exports = {
     plugins: [
+        {
+            resolve: 'gatsby-source-contentful',
+            options: {
+                spaceId: process.env.CONTENTFUL_SPACE_ID,
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+            }
+        },
         'gatsby-plugin-sass',
         {
             resolve: 'gatsby-source-filesystem',
