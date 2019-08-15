c677cfaac1dbf84ecbc47fcebf6e34282c19ffe2
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-15 15:35:29 -0700

    add a custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+export default function NotFound() {
+    return (
+        <Layout>
+            <h1>Page not found</h1>
+            <p><Link to="/">Head home</Link></p>
+        </Layout>
+    )
+}
